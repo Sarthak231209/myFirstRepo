d2e0e250a95f126b9e23f85a7b2471d52755442d
Sarthak231209 <99803679+Sarthak231209@users.noreply.github.com>
2022-02-17 14:48:51 +0530

    Create sarthak.py

---
+++ sarthak.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age : "))
+if(age>=18):
+    print ("Congratulations", name, ". You are eligible to vote.")
+else:
+    print ("Sorry", name, ". You are not eligible to vote.")
+    
